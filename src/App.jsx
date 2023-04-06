e93dc245e24516c9d13b3e80d879a9c54823f679
Axel Paillaud <a.paillaud75@gmail.com>
2023-04-06 17:29:20 +0200

    router init

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello, world!</div>,
+  },
+]);
+
 function App() {
   return (
     <div>
-      hello, world!
+      <RouterProvider router={router} />
     </div>
   );
 }
