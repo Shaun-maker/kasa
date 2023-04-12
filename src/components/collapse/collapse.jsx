6a4bc7084ea2d7485caf6c3236c6029cb38d0107
Axel Paillaud <a.paillaud75@gmail.com>
2023-04-12 06:15:37 +0200

    collapse can open and close, add also CSS animatino to it

--- src/components/collapse/collapse.jsx
+++ src/components/collapse/collapse.jsx
@@ -1,18 +1,26 @@
+import { useState } from 'react';
 import chevronDown from '../../assets/icons/chevron-down-white.svg';
 import chevronUp from '../../assets/icons/chevron-up-white.svg';
 import './collapse.css';
 
-// see if it is possible to update className with props, for margin in main container
 function Collapse({ collapseMargin }) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className={`collapse-container ${collapseMargin}`}>
-      <div className="collapse-banner">
+      <div
+        className="collapse-banner"
+        onClick={() => (isOpen ? setIsOpen(false) : setIsOpen(true))}
+      >
         <p className="collapse-title">Fiabilité</p>
         <figure className="collapse-container-img">
-          <img className="collapse-img" src={chevronUp} alt="" />
+          <img
+            className="collapse-img"
+            src={isOpen ? chevronUp : chevronDown}
+            alt=""
+          />
         </figure>
       </div>
-      <p className="collapse-content">
+      <p className={`collapse-content ${isOpen ? 'open' : 'closed'}`}>
         La bienveillance fait partie des valeurs fondatrices de Kasa. Tout
         comportement discriminatoire ou de perturbation du voisinage entraînera
         une exclusion de notre plateforme.
