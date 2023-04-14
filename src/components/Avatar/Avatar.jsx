c9b99baf7d0d017326490c65c5b3071ef3490a99
Axel Paillaud <a.paillaud75@gmail.com>
2023-04-14 16:06:43 +0200

    fix problem : have name on two line instead of one

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,9 +1,23 @@
 import './Avatar.css';
 
+function getFirstName(name) {
+  return name.split(' ')[0];
+}
+
+function getLastName(name) {
+  return name.split(' ')[1];
+}
+
 function Avatar({ name, picture }) {
+  let firstName = getFirstName(name);
+  let lastName = getLastName(name);
   return (
     <div className="avatar-container">
-      <p className="name">{name}</p>
+      <p className="name">
+        {firstName}
+        <br />
+        {lastName}
+      </p>
       <figure className="picture-container">
         <img className="picture" src={picture} alt={name} />
       </figure>
