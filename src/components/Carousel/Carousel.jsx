73415cea12cb5da3d0ecc11362950b3feceb5950
Axel Paillaud <a.paillaud75@gmail.com>
2023-04-21 12:13:31 +0200

    carousel javascript wip

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,16 +1,32 @@
 import './Carousel.css';
+import { useState } from 'react';
 import chevronLeft from '../../assets/icons/chevron-left-solid.svg';
 import chevronRight from '../../assets/icons/chevron-right-solid.svg';
 import hash from '../../assets/js/hash';
 
 function Carousel({ pictures, title }) {
+  const [isActive, setIsActive] = useState(false);
+  const buttons = document.querySelectorAll('[data-carousel-button]');
+
+  const swapCarousel = (event) => {
+    const offset = event.target.dataset.carouselButton === 'next' ? 1 : -1;
+    console.log(offset);
+  };
   return (
     <section className="carousel" data-carousel>
       <div>
-        <button data-carousel-button="prev" className="carousel-button prev">
+        <button
+          onClick={swapCarousel}
+          data-carousel-button="prev"
+          className="carousel-button prev"
+        >
           <img src={chevronLeft} alt="button prev" />
         </button>
-        <button data-carousel-button="next" className="carousel-button next">
+        <button
+          onClick={swapCarousel}
+          data-carousel-button="next"
+          className="carousel-button next"
+        >
           <img src={chevronRight} alt="button next" />
         </button>
         <span className="carousel-index">1/4</span>
